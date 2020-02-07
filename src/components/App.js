0c25871e092e5515f1dd225261d4b47452aea01c
Jonathan Forney <jonathan.forney@gmail.com>
2020-02-06 22:20:14 -0800

    these files are a hot mess

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import Counter from './counter/Counter';
+import store from '../store';
 
 function App() {
   return (
     <div className="App">
       <h1>what up chumps!</h1>
+      <Counter
+        value={store.getState()}
+        onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+        onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+        />
     </div>
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import store from './store';
+// import { Provider } from 'react-redux';
+import counter from './reducers/index';
+import Counter from './components/counter/Counter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // <Provider store={store}>
+  //   <App />
+  // </Provider>
+  <Counter
+  value={store.getState()}
+  onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+  onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+  />,
+  document.getElementById('root'));
 
+store.subscribe(ReactDOM.render);
 
