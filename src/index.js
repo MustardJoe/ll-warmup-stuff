ce14cd789ab8522ebad5a40a5872e699558a8159
Jonathan Forney <jonathan.forney@gmail.com>
2020-03-04 21:53:37 -0800

    removed unused import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM, {render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import store from './store';
 import { Provider } from 'react-redux';
