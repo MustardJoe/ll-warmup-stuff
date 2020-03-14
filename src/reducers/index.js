cedadbf0e20ef557e85cba01a4c7f327b8c81c18
Jonathan Forney <jonathan.forney@gmail.com>
2020-03-13 20:45:11 -0700

    working on reducers but having some issues

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,12 @@ import { combineReducers } from 'redux';
 import todos from './todos';
 import visibilityFilter from './visibilityFilter';
 import counter from './counter';
+import { childIds, node } from './tree';
 
 export default combineReducers({
   todos,
   counter,
-  visibilityFilter
+  visibilityFilter,
+  childIds,
+  node
 });
